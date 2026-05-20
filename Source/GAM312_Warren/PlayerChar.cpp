40c8ccfc3d01e077bd6fc989a5361012558f9871
Seth Warren <seth.warren@snhu.edu>
2026-05-20 12:03:56 -0400

    Player Stats and Resources

--- Source/GAM312_Warren/PlayerChar.cpp
+++ Source/GAM312_Warren/PlayerChar.cpp
@@ -17,6 +17,12 @@ APlayerChar::APlayerChar()
 
 	// Share rotation with controller
 	PlayerCamComp->bUsePawnControlRotation = true;
+
+	// Setup resource and resource name array
+	ResourcesArray.SetNum(3);
+	ResourcesNameArray.Add(TEXT("Wood"));
+	ResourcesNameArray.Add(TEXT("Stone"));
+	ResourcesNameArray.Add(TEXT("Berry"));
 }
 
 // Called when the game starts or when spawned
@@ -24,6 +30,9 @@ void APlayerChar::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// Create timer handle for decreasing player stats
+	FTimerHandle StatsTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(StatsTimerHandle, this, &APlayerChar::DecreaseStats, 2.0f, true);
 }
 
 // Called every frame
@@ -72,5 +81,128 @@ void APlayerChar::StopJump()
 
 void APlayerChar::FindObject()
 {
+	// Setup line trace
+	FHitResult HitResult;
+	FVector StartLocation = PlayerCamComp->GetComponentLocation();
+	FVector Direction = PlayerCamComp->GetForwardVector() * 800.0f;
+	FVector EndLocation = StartLocation + Direction;
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+	QueryParams.bTraceComplex = true;
+	QueryParams.bReturnFaceIndex = true;
+
+	// Perform line trace
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams))
+	{
+		// Cast to resource actor
+		AResource_M* HitResource = Cast<AResource_M>(HitResult.GetActor());
+
+		if (Stamina >= 5.0f)
+		{
+			if (HitResource)
+			{
+				FString hitName = HitResource->resourceName;
+				int resourceValue = HitResource->resourceAmount;
+
+				// Collect resource
+				if (HitResource->totalResource > resourceValue)
+				{
+					// Give player resource and reduce resource's total amount
+					GiveResource(resourceValue, hitName);
+					HitResource->totalResource = HitResource->totalResource - resourceValue;
+
+					check(GEngine != nullptr);
+					GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Collected"));
+
+					UGameplayStatics::SpawnDecalAtLocation(GetWorld(), hitDecal, FVector(10.0f, 10.0f, 10.f), HitResult.Location, FRotator(-90, 0, 0), 2.0f);
+
+					SetStamina(-5.0f);
+				}
+				else
+				{
+					// Give player remaining resource and destroy resource
+					GiveResource(HitResource->totalResource, hitName);
+					HitResource->Destroy();
+
+					check(GEngine != nullptr);
+					GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Depleted"));
+
+					SetStamina(-5.0f);
+				}
+			}
+		}
+	}
 }
 
+void APlayerChar::SetHealth(float amount)
+{
+	if (Health + amount < 100)
+	{
+		Health = Health + amount;
+	}
+	// If health plus amount added exceeds or equals 100, set health to 100
+	else if (Health + amount >= 100)
+	{
+		Health = 100;
+	}
+}
+
+void APlayerChar::SetHunger(float amount)
+{
+	if (Hunger + amount < 100)
+	{
+		Hunger = Hunger + amount;
+	}
+	// If hunger plus amount added exceeds or equals 100, set hunger to 100
+	else if (Hunger + amount >= 100)
+	{
+		Hunger = 100;
+	}
+}
+
+void APlayerChar::SetStamina(float amount)
+{
+	if (Stamina + amount < 100)
+	{
+		Stamina = Stamina + amount;
+	}
+	// If stamina plus amount added exceeds or equals 100, set stamina to 100
+	else if (Stamina + amount >= 100)
+	{
+		Stamina = 100;
+	}
+}
+
+void APlayerChar::DecreaseStats()
+{
+	if (Hunger > 0)
+	{
+		SetHunger(-1.0f);
+	}
+
+	SetStamina(10.0f);
+
+	if (Hunger <= 0)
+	{
+		SetHealth(-3.0f);
+	}
+}
+
+void APlayerChar::GiveResource(float amount, FString resourceType)
+{
+	if (resourceType == "Wood")
+	{
+		ResourcesArray[0] = ResourcesArray[0] + amount;
+	}
+
+	if (resourceType == "Stone")
+	{
+		ResourcesArray[1] = ResourcesArray[1] + amount;
+	}
+
+	if (resourceType == "Berry")
+	{
+		ResourcesArray[2] = ResourcesArray[2] + amount;
+	}
+}
